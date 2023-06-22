3a2d08495a6902706b843fac265f18fc4ff04ff1
patrickwide <patrickwide254@gmail.com>
2023-06-22 06:17:28 +0300

    Complete lab

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :sufficiently_clickbait_title
+
+    private
+    
+    def sufficiently_clickbait_title
+        unless title.present? && title.match?(/(Won't Believe|Secret|Top \d+|Guess)/)
+            errors.add(:title, "must be sufficiently clickbait-y")
+        end
+    end
 end
